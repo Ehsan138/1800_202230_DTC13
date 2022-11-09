7eeff34b7331b8139103514b5b618fd82eb1f18e
annettelam <113328885+annettelam@users.noreply.github.com>
2022-11-09 01:22:29 -0800

    Update categorize page

--- categorize.js
+++ categorize.js
@@ -1,33 +1,33 @@
-let hikeID = localStorage.getItem("hikeID");
+// let hikeID = localStorage.getItem("hikeID");
 
-db.collection("hikes").where("code", "==", hikeID)
-    .get()
-    .then(queryHike => {
-        //see how many results you have got from the query
-        size = queryHike.size;
-        // get the documents of query
-        Hikes = queryHike.docs;
+// db.collection("hikes").where("code", "==", hikeID)
+//     .get()
+//     .then(queryHike => {
+//         //see how many results you have got from the query
+//         size = queryHike.size;
+//         // get the documents of query
+//         Hikes = queryHike.docs;
 
-        // We want to have one document per hike, so if the the result of 
-        //the query is more than one, we can check it right now and clean the DB if needed.
-        if (size = 1) {
-            var thisHike = Hikes[0].data();
-            name = thisHike.name;
-            document.getElementById("HikeName").innerHTML = name;
-        } else {
-            console.log("Query has more than one data")
-        }
-    })
-    .catch((error) => {
-        console.log("Error getting documents: ", error);
-    });
+//         // We want to have one document per hike, so if the the result of 
+//         //the query is more than one, we can check it right now and clean the DB if needed.
+//         if (size = 1) {
+//             var thisHike = Hikes[0].data();
+//             name = thisHike.name;
+//             document.getElementById("HikeName").innerHTML = name;
+//         } else {
+//             console.log("Query has more than one data")
+//         }
+//     })
+//     .catch((error) => {
+//         console.log("Error getting documents: ", error);
+//     });
 
 
-function writeReview() {
+function enterHabit() {
     console.log("in")
-    let Title = document.getElementById("title").value;
-    let Level = document.getElementById("level").value;
-    console.log(Title, Level);
+    let ManualHabit = document.getElementById("manualhabit").value;
+    let SelectHabit = document.getElementById("selecthabit").value;
+    console.log(ManualHabit, SelectHabit);
 
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
@@ -37,11 +37,10 @@ function writeReview() {
             currentUser.get()
                 .then(userDoc => {
                     var userEmail = userDoc.data().email;
-                    db.collection("Reviews").add({
-                        code: hikeID,
+                    db.collection("habits").add({
                         userID: userID,
-                        title: Title,
-                        level: Level,
+                        manualhabit: ManualHabit,
+                        selecthabit: SelectHabit,
                         timestamp: firebase.firestore.FieldValue.serverTimestamp()
                     }).then(() => {
                         window.location.href = "viewhabits.html"; //new line added
