b110c1b6fb2dd968c35e0da59465f44f6e2704ac
Ehsan138 <67409692+Ehsan138@users.noreply.github.com>
2022-11-22 22:53:29 -0800

    adding the possible solution for updating the habit, but it doesn't work currently

--- scripts/main.js
+++ scripts/main.js
@@ -38,3 +38,83 @@ $(function() {
 $("button").click(function(){
   randomQuote();
 });
+
+
+
+
+
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        getBookmarks(user)
+    } else {
+        console.log("No user is signed in");
+    }
+});
+
+
+
+
+function getHabits(user) {
+    db.collection("users").doc(user.uid).get()
+        .then(userDoc => {
+                user.id.forEach(thisHabitID => {
+                console.log(thisHabitID);
+
+                db.collection("habits").where("code", "==", thisHabitID).get().then(snap => {
+                    bool = snap.bool;
+                    queryData = snap.docs;
+                    
+                    if (bool == true) {
+                        var doc = queryData[0].data();
+                        var habitName = doc.name
+                        var habitDescription = doc.description
+                        let newCard = CardTemplate.content.cloneNode(true);
+                        newCard.querySelector('.card-title').innerHTML = habitName;
+                        newCard.querySelector('.card-text').innerHTML = habitDescription;
+                        habitCardGroup.appendChild(newCard);
+                    } else {
+                        console.log("Query has more than one data")
+                    }
+
+                })
+
+            });
+        })
+}
+
+
+
+
+// function getBookmarks(user) {
+//     db.collection("users").doc(user.uid).get()
+//         .then(userDoc => {
+//             var bookmarks = userDoc.data().bookmarks;
+//             console.log(bookmarks);
+
+//             let CardTemplate = document.getElementById("CardTemplate");
+//             bookmarks.forEach(thisHikeID => {
+//                 console.log(thisHikeID);
+//                 db.collection("hikes").where("code", "==", thisHikeID).get().then(snap => {
+//                     size = snap.size;
+//                     queryData = snap.docs;
+                    
+//                     if (size == 1) {
+//                         var doc = queryData[0].data();
+//                         var hikeName = doc.name; //gets the name field
+//                         var hikeID = doc.code; //gets the unique ID field
+//                         var hikeLength = doc.length; //gets the length field
+//                         let newCard = CardTemplate.content.cloneNode(true);
+//                         newCard.querySelector('.card-title').innerHTML = hikeName;
+//                         newCard.querySelector('.card-length').innerHTML = hikeLength;
+//                         newCard.querySelector('a').onclick = () => setHikeData(hikeID);
+//                         newCard.querySelector('img').src = `./images/${hikeID}.jpg`;
+//                         hikeCardGroup.appendChild(newCard);
+//                     } else {
+//                         console.log("Query has more than one data")
+//                     }
+
+//                 })
+
+//             });
+//         })
+// }
