71eec0a84eb4e4e940070942cb96d1b8b1d8ef3b
jaskarn-bhatti <jaskarnbhatti204@gmail.com>
2022-11-23 09:27:02 -0800

    Delete function added

--- settings.js
+++ settings.js
@@ -91,7 +91,52 @@ function myFunction() {
     // [END auth_sign_out]
   }
 
- 
 
 
+  function insertName() {
+    // to check if the user is logged in:
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            console.log(user.uid); // let me to know who is the user that logged in to get the UID
+            currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
+            currentUser.get().then(userDoc => {
+                //get the user name
+                var user_Goals = userDoc.data().goals;
+                console.log(user_Goals);
+                $("#goals-goes-here").text(user_Goals); //jquery
+                // document.getElementByID("name-goes-here").innerText=user_Name;
+            })
+        }
+
+    })
+}
+
+
+function deleteUserById() {
+    // to check if the user is logged in:
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            currentUser = user.uid; // will to to the firestore and go to the document of the user
+            console.log(currentUser)
+            firebase.auth().currentUser.delete()
+            db.collection("users").doc(currentUser).delete()
+            // getAuth()
+            //     .deleteUser(uid)
+            //     .then(() => {
+            //         console.log('Successfully deleted user');
+            //     })
+            //     .catch((error) => {
+            //         console.log('Error deleting user:', error);
+            //     });
+        }
+    })
+}
+
+
+
+// function deleteUserById(){
+//     currentUser = user.uid;
+//     firestore.collection("users").doc(currentUser).delete()
+//     console.log(id);
+// }
 
