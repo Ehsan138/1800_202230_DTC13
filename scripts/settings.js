b62224fb32d1e4287586365df1a7f0539b2b974b
jaskarn-bhatti <jaskarnbhatti204@gmail.com>
2022-11-28 22:40:12 -0800

    added comments to settings.js and settings.html

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,13 +1,11 @@
-
- 
-// Notification
+// test creates a new notification and sends it a certain amount of times
+// return: none
+// param: none
 function test() {
-    console.log('in');
     (async () => {
         // create and show the notification
         const showNotification = () => {
-            
-            console.log('in2');
+
             // create a new notification
             const notification = new Notification('habitory.com', {
                 body: 'Go back to habit',
@@ -16,87 +14,95 @@ function test() {
             setTimeout(() => {
                 notification.close();
             }, 5 * 1000);
-   
+
             // navigate to a URL when clicked
             notification.addEventListener('click', () => {
-   
+
                 window.open('./main.html');
             });
         }
-   
+
         // show an error message
         const showError = () => {
             const error = document.querySelector('.error');
             error.style.display = 'block';
             error.textContent = 'You blocked the notifications';
         }
-   
+
         // check notification permission
         let granted = false;
-   
+
         if (Notification.permission === 'granted') {
             granted = true;
         } else if (Notification.permission !== 'denied') {
             let permission = await Notification.requestPermission();
             granted = permission === 'granted' ? true : false;
         }
-   
+
         // show notification or error
         granted ? showNotification() : showError();
- 
-        // Open
+
     })();
-   
-  }
-  var i = 1;                  
- 
+}
+
+
+var iter = 1;
+// myloop is called when the button is pressed. It loops 5 times to give the user a notification 5 times.
+// return: none
+// pram: none                
 function myLoop() {
-    console.log('ii');        
-    setTimeout(function() { 
-      console.log('hi');
-      test();  
-      i++;            
-      if (i < 5) {        
-        myLoop();            
-      }            
-    }, 10000)                    
-  }
- 
- 
- 
-  let btn = document.getElementById("btn");
-  btn.addEventListener('click', event => {
-    console.log('gi')
+    setTimeout(function () {
+        console.log('hi');
+        test();
+        iter++;
+        if (iter < 5) {
+            myLoop();
+        }
+    }, 10000)
+}
+
+
+// Button action call loop and starts the notification process
+let btn = document.getElementById("btn");
+btn.addEventListener('click', event => {
     myLoop();
-  });
- 
- 
- 
-//Logout
+});
+
+
+
+// myFunction logs out the current user from the app.
+// return: none
+// pram: none
 function myFunction() {
     // [START auth_sign_out]
     firebase.auth().signOut().then(() => {
-      // Sign-out successful.
+        // Sign-out successful.
     }).catch((error) => {
-      // An error happened.
+        // An error happened.
     });
     // [END auth_sign_out]
-  }
- 
- 
- 
- 
- 
- // Delete account
-// currentUser = user.uid
- 
-function new23(){
+}
+
+
+
+
+
+// Delete account process
+
+
+// deleteFromFirebase deletes user from the firebase auth and sends an alert warning them of this change
+// return: none
+// pram : none
+function deleteFromFirebase() {
     console.log(currentUser)
     firebase.auth().currentUser.delete()
+    alert("Account deleted logout")
 }
- 
- 
- 
+
+
+// deleteUserById deletes the user from the firestore and then call the deleteFromFirebase() function
+// return: none
+// pram: none
 function deleteUserById() {
     // to check if the user is logged in:
     firebase.auth().onAuthStateChanged(user => {
@@ -106,9 +112,8 @@ function deleteUserById() {
             console.log(currentUser)
             db.collection('users').doc(currentUser).delete();
             console.log(db.collection('users').doc(currentUser))
-            new23()
+            deleteFromFirebase()
             // alert("Account deleted logout")
         }
-    }
-    )
+    })
 }
